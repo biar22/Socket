984e42c43920fddbf1b000de4fba1d045e1cbf31
biar22 <fra.biarella03@gmail.com>
2022-04-01 15:24:03 +0200

    primo

---
+++ src/main/java/com/mycompany/gestionesocket/GestioneSocket.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.gestionesocket;
+
+import java.io.*;
+import java.net.*;
+
+
+
+/**
+ *
+ * @author frabi
+ */
+public class GestioneSocket {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
